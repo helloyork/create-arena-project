065248a0141e8fe4d5d6b280b76b3a82a1100ba4
York <helloyork@icloud.com>
2024-07-12 08:43:06 -0700

    update

--- src/assets/sources.js
+++ src/assets/sources.js
@@ -28,7 +28,7 @@ const sources = [
     {
         type: "file",
         path: "./.gitignore",
-        src: "src/assets/.gitignore"
+        src: "src/assets/.gitignore_"
     },
     {
         type: "file",
